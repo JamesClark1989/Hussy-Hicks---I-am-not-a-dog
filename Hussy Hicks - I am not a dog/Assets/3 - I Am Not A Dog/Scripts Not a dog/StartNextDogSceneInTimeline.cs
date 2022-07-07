939918e8187defd891cf34d7d0edd118339e61d6
JamesClark1989 <57790550+JamesClark1989@users.noreply.github.com>
2022-07-07 17:20:26 +1000

    some random changes. Project now in Project Hussy

---
+++ Hussy Hicks - I am not a dog/Assets/3 - I Am Not A Dog/Scripts Not a dog/StartNextDogSceneInTimeline.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartNextDogSceneInTimeline : MonoBehaviour
+{
+    [SerializeField] string sceneName;
+    [SerializeField] GameObject loadingScreen;
+    [SerializeField] Slider loadingProgressBar;
+
+    List<AsyncOperation> scenesToLoad = new List<AsyncOperation>();
+
+    public void LoadTheNextScene()
+    {
+        loadingScreen.SetActive(true);
+        scenesToLoad.Add(SceneManager.LoadSceneAsync(sceneName));
+        StartCoroutine(LoadingScreen());
+        //SceneManager.LoadScene(sceneName);
+    }
+
+    IEnumerator LoadingScreen()
+    {
+        float totalProgress = 0;
+        for(int i = 0; i < scenesToLoad.Count; ++i)
+        {
+            while (!scenesToLoad[i].isDone)
+            {
+                totalProgress += scenesToLoad[i].progress;
+                loadingProgressBar.value = totalProgress / scenesToLoad.Count;
+                yield return null;
+            }
+        }
+    }
+}
